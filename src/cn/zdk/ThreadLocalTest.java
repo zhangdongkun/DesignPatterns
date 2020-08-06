dc352fa0e23eabbc5703d7e3233d8d1fe792ff12
zhangdongkun <zhangdongkun819@gmail.com>
2020-08-06 10:09:28 +0800

    TreaLocalTest

--- src/cn/zdk/ThreadLocalTest.java
+++ src/cn/zdk/ThreadLocalTest.java
@@ -1,5 +1,8 @@
 package cn.zdk;
 
+/**
+ * 应该先set 再 get，否则应该重新 initialValue 方法 完成初始化
+ */
 public class ThreadLocalTest implements  Runnable {
     //匿名类不类实现初始化
     private  static  ThreadLocal<Integer> index =  new ThreadLocal(){
@@ -12,6 +15,7 @@ public class ThreadLocalTest implements  Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 100;i++) {
+
             Integer j = index.get() + 1;
             index.set(j);
             System.out.println("当前线程：" + Thread.currentThread().getId() + "-->i:" + j);
